d745beffc912908f1ccf0bb3142ed88f2ab63f85
gteix <gabriel.silveira@dcomp.ufs.br>
2023-08-14 15:33:08 -0300

    Adicionei os construtores das classes vertice e grafo

--- src/Grafo.java
+++ src/Grafo.java
@@ -1,3 +1,35 @@
+import java.util.*;
+
 public class Grafo
 {
+    private int numVertices;
+    private List<Vertice> vertices;
+    private int numArestas;
+    private List<Aresta> arestas;
+    private Map<Vertice, List<Vertice>> listaAdj;
+    private boolean matriz;
+    private int[][] matrizAdj;
+
+    public Grafo(int numVertices, boolean matriz)
+    {
+        this.numArestas = 0;
+        this.numVertices = numVertices;
+        this.matriz = matriz;
+        if (matriz){
+            matrizAdj = new int[numVertices][numVertices];
+        }
+        else {
+            this.listaAdj = new HashMap<>();
+        }
+        this.arestas = new LinkedList<>();
+        this.vertices = new LinkedList<>();
+    }
+    void adicionarVertice(int indice, String rotulo) {
+        listaAdj.put(new Vertice(indice, rotulo), new LinkedList());
+    }
+
+
+
+
+
 }
--- src/Vertice.java
+++ src/Vertice.java
@@ -1,4 +1,53 @@
 public class Vertice
 {
+    private String rotulo;
+    private int indice;
+    private int grau;
+    private boolean flag;
 
+
+    public Vertice(int indice, String rotulo)
+    {
+        setIndice(indice);
+        setRotulo(rotulo);
+        setGrau(0);
+        setFlag(false);
+    }
+    public String getRotulo() {
+        return rotulo;
+    }
+
+    public void setRotulo(String rotulo) {
+        this.rotulo = rotulo;
+    }
+
+    public int getIndice() {
+        return indice;
+    }
+
+    public void setIndice(int indice) {
+        this.indice = indice;
+    }
+
+    public int getGrau() {
+        return grau;
+    }
+
+    public void setGrau(int grau) {
+        this.grau = grau;
+    }
+    public void incrementarGrau() {
+        this.grau++;
+    }
+    public void decrementarGrau() {
+        this.grau--;
+    }
+
+    public boolean getFlag() {
+        return flag;
+    }
+
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
 }
